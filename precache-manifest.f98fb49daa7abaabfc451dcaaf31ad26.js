c2f8244f53a82b7dd5ad5b363cb5144492700ca6
Jorge Rivera <jrdeveloper71@gmail.com>
2019-08-02 16:38:36 -0400

    Updates

---
+++ precache-manifest.f98fb49daa7abaabfc451dcaaf31ad26.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c2f3f0d365c94def37827ebfaf0141b",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "513faaf858df96966188",
+    "url": "/monster-rolodex/static/css/main.ece4bc32.chunk.css"
+  },
+  {
+    "revision": "511a6eb6b7ec2e1e4ec3",
+    "url": "/monster-rolodex/static/js/2.26ba37d3.chunk.js"
+  },
+  {
+    "revision": "513faaf858df96966188",
+    "url": "/monster-rolodex/static/js/main.43e9daf4.chunk.js"
+  },
+  {
+    "revision": "344166cfeaf30c2159bd",
+    "url": "/monster-rolodex/static/js/runtime~main.5df944a8.js"
+  }
+]);
